99f0e2b1b63a66f4584b2528513fd501f47ff813
Pedro Guedes <mobile.gds@gmail.com>
2016-07-01 20:24:35 -0300

    App Final!

--- app/src/main/java/me/pedroguedes/courtcounter/MainActivity.java
+++ app/src/main/java/me/pedroguedes/courtcounter/MainActivity.java
@@ -8,12 +8,14 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     /**
      * Displays the given score for Team A.
@@ -24,35 +26,74 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Called when the three points is clicked.
+     * Displays the given score for Team B.
      */
-    public void addThreePointsForA (View view) {
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+    /**
+     * Called when the three points for A is clicked.
+     */
+    public void addThreePointsForA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
 
     }
 
     /**
-     * Called when the two points is clicked.
+     * Called when the three points for B is clicked.
      */
-    public void addTwoPointsForA (View view) {
+    public void addThreePointsForB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+
+    }
+
+
+    /**
+     * Called when the two points for A is clicked.
+     */
+    public void addTwoPointsForA(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
     /**
-     * Called when the one points is clicked.
+     * Called when the two points for B is clicked.
      */
-    public void addOnePointForA (View view) {
+    public void addTwoPointsForB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+    /**
+     * Called when the one point for A is clicked.
+     */
+    public void addOnePointForA(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
     /**
+     * Called when the one point for B is clicked.
+     */
+    public void addOnePointForB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+    /**
      * Called when the reset is clicked.
      */
-    public void reset (View view) {
+    public void reset(View view) {
         scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamB(scoreTeamB);
         displayForTeamA(scoreTeamA);
     }
 
